91724b2a4793f9ad1e69b00e98780e60479b390f
BoyanK95 <boyan_k_95@abv.bg>
2026-04-27 00:54:07 +0300

    refactor: App into HomeTabScreen

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,7 +1,7 @@
 import IntroductionSection from "@/components/IntroductionSection/IntroductionSection";
 import { View } from "react-native";
 
-export default function App() {
+export default function HomeTabScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <IntroductionSection />
